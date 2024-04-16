ba5bef8a3d49c1e2673c9d127867a518f6701ebe
zahid <zahid.aiub6@gmail.com>
2024-04-16 22:15:14 +0200

    initial commit

---
+++ src/main/java/com/github/action/GithubActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionDemoApplication.class, args);
+	}
+
+}
